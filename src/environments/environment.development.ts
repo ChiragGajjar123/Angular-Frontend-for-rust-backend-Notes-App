62e9bfec7dc638590af204aa978a66080ccd62a3
Chirag Gajjar <chiraggajjar421@gmail.com>
2026-07-25 11:47:27 +0530

    fix: Update backend API Gateway endpoint URL to new deployed stack

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://4tntxor1lk.execute-api.ap-south-1.amazonaws.com',
+  apiUrl: 'https://9err4pahv7.execute-api.ap-south-1.amazonaws.com',
 };
